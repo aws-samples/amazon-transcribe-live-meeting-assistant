93c9b8d7353c15a526887246fc5dbc104e655388
Christopher Lott <cmlott@amazon.com>
2024-04-01 22:13:41 +0000

    add required speaker field to 'other' event processor type

--- lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/eventprocessor.py
+++ lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/eventprocessor.py
@@ -331,6 +331,10 @@ def normalize_transcript_segments(message: Dict) -> List[Dict]:
         if message.get("EndTime", None):
             end_time = message["EndTime"]
         
+        speaker = message.get("Speaker", None)
+        if (not speaker):
+            speaker = "Other Participant"
+
         transcript = message["Transcript"]
         is_partial = message["IsPartial"]
         
@@ -344,6 +348,7 @@ def normalize_transcript_segments(message: Dict) -> List[Dict]:
                     StartTime=start_time,
                     EndTime=end_time,
                     Transcript=transcript,
+                    Speaker=speaker,
                     OriginalTranscript=transcript,
                     IsPartial=is_partial,
                     Sentiment=sentiment,
