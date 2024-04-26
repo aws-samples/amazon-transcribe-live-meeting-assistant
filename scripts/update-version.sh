578f51af7715cb3eb0e9c05bffc5d8651eb522a0
Christopher Lott <cmlott@amazon.com>
2024-04-26 18:56:29 +0000

    update additional version numbers

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -7,6 +7,7 @@
 set -eu -o pipefail
 
 export NEW_VERSION=${1:-}
+echo "New version: $NEW_VERSION"
 
 [[ -z "$NEW_VERSION" ]] && {
     echo "usage: $0 '<new version>'" >&2
@@ -17,6 +18,7 @@ export NEW_VERSION=${1:-}
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=$(readlink -f "${SCRIPT_DIR}/..")
 AI_STACK_DIR="${ROOT_DIR}/lma-ai-stack"
+EXTENSION_STACK_DIR="${ROOT_DIR}/lma-browser-extension-stack"
 
 # files to be modified
 # values can be overridden by existing environment vars values
@@ -27,6 +29,10 @@ AI_STACK_TEMPLATE_FILE=${AI_STACK_TEMPLATE_FILE:-"${AI_STACK_DIR}/deployment/lma
 SAMCONFIG_FILE=${SAMCONFIG_FILE:-"${AI_STACK_DIR}/samconfig.toml"}
 UI_PACKAGE_JSON_FILE=${UI_PACKAGE_JSON_FILE:-"${AI_STACK_DIR}/source/ui/package.json"}
 UI_PACKAGE_LOCK_JSON_FILE=${UI_PACKAGE_LOCK_JSON_FILE:-"${AI_STACK_DIR}/source/ui/package-lock.json"}
+
+EXTENSION_PACKAGE_JSON_FILE=${EXTENSION_PACKAGE_JSON_FILE:-"${EXTENSION_STACK_DIR}/package.json"}
+EXTENSION_MANIFEST_FILE=${EXTENSION_MANIFEST_FILE:-"${EXTENSION_STACK_DIR}/public/manifest.json"}
+
 
 export VERSION_REGEX="${VERSION_REGEX:-$'((0|[1-9]\d*)\.){2\}(0|[1-9]\d*)'}"
 
@@ -57,7 +63,7 @@ if [[ -f "$TEMPLATE_FILE" ]] ; then
     sed --in-place --regexp-extended --expression "$(
         # shellcheck disable=SC2016
         echo '
-          s/^(Description: .+\(v)${VERSION_REGEX}(\).*)$/\1${NEW_VERSION}\5/;
+        s/^(Description: .+\(v)${VERSION_REGEX}(\).*)$/\1${NEW_VERSION}\5/;
         ' | \
         envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
     )" "$TEMPLATE_FILE"
@@ -70,9 +76,9 @@ if [[ -f "$AI_STACK_TEMPLATE_FILE" ]] ; then
     sed --in-place --regexp-extended --expression "$(
         # shellcheck disable=SC2016
         echo '
-		  /^ {2,}BootstrapVersion:/ , /^ {2,}Default:/ {
+        /^ {2,}BootstrapVersion:/ , /^ {2,}Default:/ {
             s/^(.*Default: {1,})${VERSION_REGEX}(.*)/\1${NEW_VERSION}\5/;
-          }
+        }
         ' | \
         envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
     )" "$AI_STACK_TEMPLATE_FILE"
@@ -85,8 +91,8 @@ if [[ -f "$SAMCONFIG_FILE" ]] ; then
     sed --in-place --regexp-extended --expression "$(
         # shellcheck disable=SC2016
         echo '
-          s/^( *s3_prefix *=.*)${VERSION_REGEX}(.*)$/\1${NEW_VERSION}\5/;
-          s/^( *"BootstrapVersion=)${VERSION_REGEX}(.*)$/\1${NEW_VERSION}\5/;
+        s/^( *s3_prefix *=.*)${VERSION_REGEX}(.*)$/\1${NEW_VERSION}\5/;
+        s/^( *"BootstrapVersion=)${VERSION_REGEX}(.*)$/\1${NEW_VERSION}\5/;
         ' | \
         envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
     )" "$SAMCONFIG_FILE"
@@ -99,9 +105,9 @@ if [[ -f "$UI_PACKAGE_JSON_FILE" ]] ; then
     sed --in-place --regexp-extended --expression "$(
         # shellcheck disable=SC2016
         echo '
-		  /^ *"name" *: *"lma-ui" *, *$/ , /^ *"version" *:/ {
+        /^ *"name" *: *"lma-ui" *, *$/ , /^ *"version" *:/ {
             s/^(.*"version" *: *")${VERSION_REGEX}(.*)/\1${NEW_VERSION}\5/;
-          }
+        }
         ' | \
         envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
     )" "$UI_PACKAGE_JSON_FILE"
@@ -111,15 +117,45 @@ fi
 
 if [[ -f "$UI_PACKAGE_LOCK_JSON_FILE" ]] ; then
     echo "Updating version in ${UI_PACKAGE_LOCK_JSON_FILE} file"
-    sed --in-place --regexp-extended --expression "$(
+        sed --in-place --regexp-extended --expression "$(
         # shellcheck disable=SC2016
         echo '
-		  /^ *"name" *: *"lma-ui" *, *$/ , /^ *"version" *:/ {
+        /^ *"name" *: *"lma-ui" *, *$/ , /^ *"version" *:/ {
             s/^(.*"version" *: *")${VERSION_REGEX}(.*)/\1${NEW_VERSION}\5/;
-          }
+        }
         ' | \
         envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
     )" "$UI_PACKAGE_LOCK_JSON_FILE"
 else
     echo "[WARNING] ${UI_PACKAGE_LOCK_JSON_FILE} file does not exist" >&2
+fi
+
+if [[ -f "$EXTENSION_PACKAGE_JSON_FILE" ]] ; then
+    echo "Updating version in ${EXTENSION_PACKAGE_JSON_FILE} file"
+    sed --in-place --regexp-extended --expression "$(
+        # shellcheck disable=SC2016
+        echo '
+        /^ *"name" *: *"lma-extension" *, *$/ , /^ *"version" *:/ {
+            s/^(.*"version" *: *")${VERSION_REGEX}(.*)/\1${NEW_VERSION}\5/;
+        }
+        ' | \
+        envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
+    )" "$EXTENSION_PACKAGE_JSON_FILE"
+else
+    echo "[WARNING] ${EXTENSION_PACKAGE_JSON_FILE} file does not exist" >&2
+fi
+
+if [[ -f "$EXTENSION_MANIFEST_FILE" ]] ; then
+    echo "Updating version in ${EXTENSION_MANIFEST_FILE} file"
+    sed --in-place --regexp-extended --expression "$(
+        # shellcheck disable=SC2016
+        echo '
+        /^ *"name" *: *"Amazon Live Meeting Assistant" *, *$/ , /^ *"version" *:/ {
+            s/^(.*"version" *: *")${VERSION_REGEX}(.*)/\1${NEW_VERSION}\5/;
+        }
+        ' | \
+        envsubst '${VERSION_REGEX} ${NEW_VERSION}' \
+    )" "$EXTENSION_MANIFEST_FILE"
+else
+    echo "[WARNING] ${EXTENSION_MANIFEST_FILE} file does not exist" >&2
 fi
