854185334d5e873bb1feae61ceb82f13ea4c4ad8
Bob Strahan <strahans@amazon.com>
2024-10-19 21:46:06 +0000

    Apply filters for non-admin users only

--- lma-ai-stack/source/lambda_functions/query_knowledgebase_resolver/index.py
+++ lma-ai-stack/source/lambda_functions/query_knowledgebase_resolver/index.py
@@ -1,87 +1,87 @@
-import json
-import os
-import boto3
-
-print("Boto3 version: ", boto3.__version__)
-
-KB_REGION = os.environ.get("KB_REGION") or os.environ["AWS_REGION"]
-KB_ID = os.environ.get("KB_ID")
-MODEL_ID = os.environ.get("MODEL_ID")
-MODEL_ARN = f"arn:aws:bedrock:{KB_REGION}::foundation-model/{MODEL_ID}"
-
-KB_CLIENT = boto3.client(
-    service_name="bedrock-agent-runtime",
-    region_name=KB_REGION
-)
-
-def get_kb_response(query, userId, sessionId):
-    input = {
-        "input": {
-            'text': query
+{
+    "identity": {
+        "claims": {
+            "sub": "44f8c408-0081-70ca-16f1-f25e57f345aa",
+            "cognito:groups": [
+                "Admin"
+            ],
+            "iss": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3OaYtZt7v",
+            "client_id": "20nmbrs8vcf3neo696m28huipv",
+            "origin_jti": "e7182c23-be34-4729-8d92-4edb610cd317",
+            "event_id": "d5ae5d0e-638f-46dc-b4a6-b6ebe6ca5bac",
+            "token_use": "access",
+            "scope": "aws.cognito.signin.user.admin",
+            "auth_time": 1729177241,
+            "exp": 1729375540,
+            "iat": 1729371940,
+            "jti": "6f3f07b6-47a0-45e7-803a-d6dd06650cc5",
+            "username": "strahanr@amazon.com"
         },
-        "retrieveAndGenerateConfiguration": {
-            'knowledgeBaseConfiguration': {
-                'knowledgeBaseId': KB_ID,
-                'modelArn': MODEL_ARN,
-                "retrievalConfiguration": {
-                    "vectorSearchConfiguration": {
-                        "filter": {
-                            "equals": {
-                                "key": "Owner",
-                                "value": userId
-                            }
-                        }
-                    }
-                }
-            },
-            'type': 'KNOWLEDGE_BASE'
+        "defaultAuthStrategy": "ALLOW",
+        "groups": [
+            "Admin"
+        ],
+        "issuer": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3OaYtZt7v",
+        "sourceIp": [
+            "68.100.102.141"
+        ],
+        "sub": "44f8c408-0081-70ca-16f1-f25e57f345aa",
+        "username": "strahanr@amazon.com"
+    },
+    "result": null,
+    "request": {
+        "headers": {
+            "x-forwarded-for": "68.100.102.141, 130.176.134.152",
+            "sec-ch-ua-mobile": "?0",
+            "cloudfront-viewer-country": "US",
+            "cloudfront-is-tablet-viewer": "false",
+            "x-amzn-requestid": "6808de5c-33d2-43f5-87b8-465f60503654",
+            "via": "2.0 657550dfe2740440cd2d2c5a84cf231e.cloudfront.net (CloudFront)",
+            "cloudfront-forwarded-proto": "https",
+            "origin": "https://d10g2tsgjlecfi.cloudfront.net",
+            "content-length": "170",
+            "x-forwarded-proto": "https",
+            "accept-language": "en-US,en;q=0.9",
+            "host": "kuayw4vwcjdajhpwqqgrgookny.appsync-api.us-east-1.amazonaws.com",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            "cloudfront-is-mobile-viewer": "false",
+            "accept": "application/json, text/plain, */*",
+            "priority": "u=1, i",
+            "cloudfront-viewer-asn": "22773",
+            "cloudfront-is-smarttv-viewer": "false",
+            "x-amzn-appsync-is-vpce-request": "false",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "referer": "https://d10g2tsgjlecfi.cloudfront.net/",
+            "x-amzn-remote-ip": "68.100.102.141",
+            "content-type": "application/json; charset=UTF-8",
+            "sec-fetch-mode": "cors",
+            "x-amzn-trace-id": "Root=1-671427cf-6b851506684e208e2750e11f",
+            "x-amz-cf-id": "5YCHFM889jRZyeXYfl-S-1Bhj_CiZyPLdCEYY51XYbX5V9GXEQaAMg==",
+            "authorization": "eyJraWQiOiJQTFJtQXpuYm00Wko0WjdHT2FQemQxUWtOcUR6UjJVN2xnXC9mXC9iY2dENmc9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI0NGY4YzQwOC0wMDgxLTcwY2EtMTZmMS1mMjVlNTdmMzQ1YWEiLCJjb2duaXRvOmdyb3VwcyI6WyJBZG1pbiJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV8zT2FZdFp0N3YiLCJjbGllbnRfaWQiOiIyMG5tYnJzOHZjZjNuZW82OTZtMjhodWlwdiIsIm9yaWdpbl9qdGkiOiJlNzE4MmMyMy1iZTM0LTQ3MjktOGQ5Mi00ZWRiNjEwY2QzMTciLCJldmVudF9pZCI6ImQ1YWU1ZDBlLTYzOGYtNDZkYy1iNGE2LWI2ZWJlNmNhNWJhYyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MjkxNzcyNDEsImV4cCI6MTcyOTM3NTU0MCwiaWF0IjoxNzI5MzcxOTQwLCJqdGkiOiI2ZjNmMDdiNi00N2EwLTQ1ZTctODAzYS1kNmRkMDY2NTBjYzUiLCJ1c2VybmFtZSI6InN0cmFoYW5yQGFtYXpvbi5jb20ifQ.VAj_UhYq6D1pKj0fQq8gjUWwkNj-JV6RPfcc4yMuEP7aRNO9vSguKAb76LUjkvrnKf9YwhJLxFhEO6Vh90OB3Sci1xGRN_hQvCSXuyKbqUHRT7AGSAOW3jyRod_s-FiBT89_ure-HYBN9ASqaZfgN19zt27HihrVy429fEbjHltWai6rEBO7vfeUYn341DJb36SdUSJvVNrCXqAeC93dBqiYxWvUIFVuLVHDyi-0sGrUz7FBo5oTCjVfLm6P5VqmCwh0TEd1_7D34P7uyi3Mb963PBWz6_NPZlR1YAIsAciyi2IhRd6jN0H7kOyySRCbfISzYDA_0kG0jIFRrm4j8Q",
+            "sec-fetch-dest": "empty",
+            "x-amz-user-agent": "aws-amplify/4.7.14 js",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "cloudfront-is-desktop-viewer": "true",
+            "sec-fetch-site": "cross-site",
+            "sec-ch-ua": "\"Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+            "x-forwarded-port": "443"
+        },
+        "domainName": null
+    },
+    "info": {
+        "fieldName": "queryKnowledgeBase",
+        "parentTypeName": "Query",
+        "variables": {
+            "input": "who else attended?"
         }
-    }
-    if sessionId:
-        input["sessionId"] = sessionId
-    print("Amazon Bedrock KB Request: ", input)
-    try:
-        resp = KB_CLIENT.retrieve_and_generate(**input)
-    except Exception as e:
-        print("Amazon Bedrock KB Exception: ", e)
-        resp = {
-            "systemMessage": "Amazon Bedrock KB Error: " + str(e)
-        }
-    print("Amazon Bedrock KB Response: ", json.dumps(resp))
-    return resp
-
-
-def markdown_response(kb_response):
-    showContextText = True
-    message = kb_response.get("output", {}).get("text", {}) or kb_response.get(
-        "systemMessage") or "No answer found"
-    markdown = message
-    if showContextText:
-        contextText = ""
-        sourceLinks = []
-        for source in kb_response.get("citations", []):
-            for reference in source.get("retrievedReferences", []):
-                snippet = reference.get("content", {}).get(
-                    "text", "no reference text")
-                callId = reference.get("metadata",{}).get("CallId")
-                url = f"{callId}"
-                title = callId
-                contextText = f'{contextText}<br><callid href="{url}">{title}</callid><br>{snippet}\n'
-                sourceLinks.append(f'<callid href="{url}">{title}</callid>')
-        if contextText:
-            markdown = f'{markdown}\n<details><summary>Context</summary><p style="white-space: pre-line;">{contextText}</p></details>'
-        if len(sourceLinks):
-            markdown = f'{markdown}<br>Sources: ' + ", ".join(sourceLinks)
-    return markdown
-
-
-def handler(event, context):
-    print("Received event: %s" % json.dumps(event))
-    query = event["arguments"]["input"]
-    sessionId = event["arguments"].get("sessionId") or None
-    userId = event["identity"]["username"]
-    # future enhancements could allow additional metadata filters specified in the UI
-    kb_response = get_kb_response(query, userId, sessionId)
-    kb_response["markdown"] = markdown_response(kb_response)
-    print("Returning response: %s" % json.dumps(kb_response))
-    return json.dumps(kb_response)
+    },
+    "error": null,
+    "prev": null,
+    "stash": {},
+    "outErrors": [],
+    "arguments": {
+        "input": "who else attended?",
+        "sessionId": null
+    },
+    "source": null
+}
