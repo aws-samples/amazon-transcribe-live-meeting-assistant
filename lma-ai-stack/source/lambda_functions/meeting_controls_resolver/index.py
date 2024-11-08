82fcdd03ba201f5c5ddd0ab83ac5299e046f3aa3
Kishore Dhamodaran <kkdaws@amazon.com>
2024-11-08 07:42:52 -0600

    Move logic to use AppSync for sharing calls in the meeting controls Lambda

--- lma-ai-stack/source/lambda_functions/meeting_controls_resolver/index.py
+++ lma-ai-stack/source/lambda_functions/meeting_controls_resolver/index.py
@@ -22,6 +22,79 @@ logger = logging.getLogger(__name__)
 ddb = boto3.resource('dynamodb')
 ddbTable = ddb.Table(LCA_CALL_EVENTS_TABLE)
 
+### Common functions
+
+def get_call_details(appsync_session, schema, callid):
+    try:
+        query = dsl_gql(
+            DSLQuery(
+                schema.Query.getCall.args(CallId=callid).select(
+                    schema.Call.PK,
+                    schema.Call.SK,
+                    schema.Call.CallId,
+                    schema.Call.CreatedAt,
+                    schema.Call.Owner,
+                    schema.Call.SharedWith
+                )
+            )
+        )
+
+        result = appsync_session.execute(query)
+        print("get call result", result)
+    except ClientError as err:
+        logger.error("Error getting call details %s: %s",
+                     err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+    else:
+        return result
+
+def get_transcript_segments(appsync_session, schema, callid):
+    try:
+        query = dsl_gql(
+            DSLQuery(
+                schema.Query.getTranscriptSegments.args(callId=callid).select(
+                    schema.TranscriptSegmentList.TranscriptSegments.select(
+                        schema.TranscriptSegment.PK,
+                        schema.TranscriptSegment.SK,
+	                    schema.TranscriptSegment.CreatedAt,
+                        schema.TranscriptSegment.CallId,
+                        schema.TranscriptSegment.SegmentId,
+                        schema.TranscriptSegment.StartTime,
+                        schema.TranscriptSegment.EndTime,
+                        schema.TranscriptSegment.Transcript,
+                        schema.TranscriptSegment.IsPartial,
+                        schema.TranscriptSegment.Channel,
+                        schema.TranscriptSegment.Speaker,
+                    )
+                )
+            )
+        )
+
+        result = appsync_session.execute(query)
+        query_string = print_ast(query)
+        print("get transcript segments result", query_string, result)
+    except ClientError as err:
+        logger.error("Error deleting meetings %s: %s",
+                     err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+    else:
+        return result
+
+def get_call_metadata(callid):
+    pk = 'c#'+callid
+    print(f"Call metadata PK: {pk}")
+    try:
+        metadata = ddbTable.get_item(
+            Key={'PK': pk, 'SK': pk},
+            TableName=LCA_CALL_EVENTS_TABLE
+            )
+    except ClientError as err:
+        logger.error("Error getting metadata from LCA Call Events table %s: %s",
+                     err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+    else:
+        return metadata['Item']
+
 def verify_permissions(event):
     request_owner = event["identity"]["username"]
     isAdminUser = False
@@ -40,6 +113,99 @@ def verify_permissions(event):
             return False
 
     return True
+
+## Share Meetings
+
+def share_meetings(calls, owner, recipients):
+    if recipients and not all(re.match(r"[^@]+@[^@]+\.[^@]+", email) for email in recipients.split(',')):
+        return { 'Result': "Invalid email address provided" }
+
+    with appsync_client as appsync_session:
+        if not appsync_session.client.schema:
+            raise ValueError("invalid AppSync schema")
+        schema = DSLSchema(appsync_session.client.schema)
+
+        for call in calls:        
+            callid = call['CallId']
+            listPK = call['ListPK']
+            listSK = call['ListSK']
+            print("CallID: ", callid, listPK, listSK)
+            share_meeting(appsync_session, schema, callid, listPK, listSK, recipients, owner)
+    
+    callIds = [call['CallId'] for call in calls]
+    return { 'Calls': callIds, 
+             'Result': "Meetings shared successfully",
+             'Owner': owner,
+             'SharedWith': recipients 
+        }
+
+def share_meeting(appsync_session, schema, callid, listPK, listSK, recipients, owner):
+    pk = 'c#' + callid
+    new_recipients = list(set(email.strip() for email in recipients.split(',') if email.strip()))
+
+    if not recipients:
+        new_recipients = None
+
+    try:        
+        result = get_transcript_segments(appsync_session, schema, callid)
+        for transcript_segment in result.get("getTranscriptSegments").get("TranscriptSegments"):
+            update_transcript_segment(appsync_session, schema, transcript_segment["PK"], transcript_segment["SK"], new_recipients)
+
+        input = {
+            "CallId": callid,
+            "ListPK": listPK,
+            "ListSK": listSK,
+            "Owner": owner,
+            "SharedWith": new_recipients,
+        }
+
+        # Now share the call records (PK that begins with c# and cls#)
+        mutation = dsl_gql(
+            DSLMutation(
+                schema.Mutation.shareCall.args(input=input).select(
+                    schema.ShareCallOutput.CallId,
+                    schema.ShareCallOutput.Owner,
+                    schema.ShareCallOutput.SharedWith
+                )
+            )
+        )
+
+        result = appsync_session.execute(mutation)
+        print("share call query result", result)
+
+    except ClientError as err:
+        logger.error("Error updating people can access %s: %s",
+                     err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+    else:
+        return
+
+def update_transcript_segment(appsync_session, schema, PK, SK, new_recipients):
+    input = {
+        "PK": PK,
+        "SK": SK,
+        "SharedWith": new_recipients
+    }
+
+    try:
+        mutation = dsl_gql(
+            DSLMutation(
+                schema.Mutation.shareTranscriptSegment.args(input=input).select(
+                    schema.ShareTranscriptSegmentOutput.PK,
+                )
+            )
+        )
+        result = appsync_session.execute(mutation)
+        print("update transcript segment result", result)
+
+    except ClientError as err:
+        logger.error("Error updating transcript segment %s: %s",
+                     err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+    else:
+        return
+
+## Delete Meetings
 
 def delete_meetings(calls, owner):
     with appsync_client as appsync_session:
@@ -95,30 +261,6 @@ def delete_meeting(appsync_session, schema, callid, listPK, listSK, owner):
     else:
         return
 
-def get_call_details(appsync_session, schema, callid):
-    try:
-        query = dsl_gql(
-            DSLQuery(
-                schema.Query.getCall.args(CallId=callid).select(
-                    schema.Call.PK,
-                    schema.Call.SK,
-                    schema.Call.CallId,
-                    schema.Call.CreatedAt,
-                    schema.Call.Owner,
-                    schema.Call.SharedWith
-                )
-            )
-        )
-
-        result = appsync_session.execute(query)
-        print("get call result", result)
-    except ClientError as err:
-        logger.error("Error getting call details %s: %s",
-                     err.response['Error']['Code'], err.response['Error']['Message'])
-        raise
-    else:
-        return result
-
 def delete_transcript_segment(appsync_session, schema, PK, SK):
     input = {
         "PK": PK,
@@ -144,143 +286,6 @@ def delete_transcript_segment(appsync_session, schema, PK, SK):
     else:
         return
 
-def get_transcript_segments(appsync_session, schema, callid):
-    try:
-        query = dsl_gql(
-            DSLQuery(
-                schema.Query.getTranscriptSegments.args(callId=callid).select(
-                    schema.TranscriptSegmentList.TranscriptSegments.select(
-                        schema.TranscriptSegment.PK,
-                        schema.TranscriptSegment.SK,
-	                    schema.TranscriptSegment.CreatedAt,
-                        schema.TranscriptSegment.CallId,
-                        schema.TranscriptSegment.SegmentId,
-                        schema.TranscriptSegment.StartTime,
-                        schema.TranscriptSegment.EndTime,
-                        schema.TranscriptSegment.Transcript,
-                        schema.TranscriptSegment.IsPartial,
-                        schema.TranscriptSegment.Channel,
-                        schema.TranscriptSegment.Speaker,
-                    )
-                )
-            )
-        )
-
-        result = appsync_session.execute(query)
-        query_string = print_ast(query)
-        print("get transcript segments result", query_string, result)
-    except ClientError as err:
-        logger.error("Error deleting meetings %s: %s",
-                     err.response['Error']['Code'], err.response['Error']['Message'])
-        raise
-    else:
-        return result
-
-def update_meeting_permissions(callid, listPK, listSK, recipients):
-    pk = 'c#' + callid
-    new_recipients = list(set(email.strip() for email in recipients.split(',') if email.strip()))
-
-    if not recipients:
-        new_recipients = None
-    try:        
-        updated_count = ddb_update_delete(pk, None, 'Share', new_recipients )
-
-        pk = 'trs#' + callid
-        updated_count += ddb_update_delete(pk, None, 'Share', new_recipients )
-
-        updated_count += ddb_update_delete(listPK, listSK, 'Share', new_recipients)
-
-        print(f"Successfully updated {updated_count} items for CallId: {callid}")
-        return
-
-    except ClientError as err:
-        logger.error("Error updating people can access %s: %s",
-                     err.response['Error']['Code'], err.response['Error']['Message'])
-        raise
-    else:
-        return
-
-def ddb_update_delete(pk, sk, action, recipients ):
-    try:
-        if (sk):
-            items = [
-                {
-                    'PK': pk,
-                    'SK': sk
-                }
-            ]
-        else:
-            response = ddbTable.query(
-                KeyConditionExpression=Key('PK').eq(pk)
-            )
-            items = response['Items']
-            
-            while 'LastEvaluatedKey' in response:
-                response = ddbTable.query(
-                    KeyConditionExpression=Key('PK').eq(pk),
-                    ExclusiveStartKey=response['LastEvaluatedKey']
-                )
-                items.extend(response['Items'])
-
-        updated_count = 0
-        for item in items:
-            if action == 'Share':
-                ddbTable.update_item(
-                    Key={
-                        'PK': item['PK'],
-                        'SK': item['SK']
-                    },
-                    UpdateExpression="SET SharedWith = :val",
-                    ExpressionAttributeValues={':val': recipients}
-                )
-            elif action == 'Delete':
-                ddbTable.delete_item(
-                    Key={
-                        'PK': item['PK'],
-                        'SK': item['SK']
-                    }
-                )
-            updated_count += 1
-    except ClientError as err:
-        logger.error("Error during update or delete for %s: %s",
-                     err.response['Error']['Code'], err.response['Error']['Message'])
-        raise
-    else:
-        return updated_count
-
-def get_call_metadata(callid):
-    pk = 'c#'+callid
-    print(f"Call metadata PK: {pk}")
-    try:
-        metadata = ddbTable.get_item(
-            Key={'PK': pk, 'SK': pk},
-            TableName=LCA_CALL_EVENTS_TABLE
-            )
-    except ClientError as err:
-        logger.error("Error getting metadata from LCA Call Events table %s: %s",
-                     err.response['Error']['Code'], err.response['Error']['Message'])
-        raise
-    else:
-        return metadata['Item']
-
-def share_meetings(calls, owner, recipients):
-    if recipients and not all(re.match(r"[^@]+@[^@]+\.[^@]+", email) for email in recipients.split(',')):
-        return { 'Result': "Invalid email address provided" }
-
-    for call in calls:        
-        callid = call['CallId']
-        listPK = call['ListPK']
-        listSK = call['ListSK']
-        print("CallID: ", callid, listPK, listSK)
-        update_meeting_permissions(callid, listPK, listSK, recipients)
-    
-    callIds = [call['CallId'] for call in calls]
-    return { 'Calls': callIds, 
-             'Result': "Meetings shared successfully",
-             'Owner': owner,
-             'SharedWith': recipients 
-        }
-
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event))
     owner = event["identity"]["username"]
@@ -297,12 +302,3 @@ def lambda_handler(event, context):
         return delete_meetings(calls, owner)
     else:
         return { 'Result': "Invalid action" }
-
-# Test case
-if __name__ == '__main__':
-    lambda_handler({
-        "CallId": "2359fb61-f612-4fe9-bce2-839061c328f9",
-        "TokenCount": 0,
-        "ProcessTranscript": False,
-        "LastNTurns": 20
-    }, {})
