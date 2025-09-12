1625262ce31edf9dfb1fc27d5438e2b40409081e
Jeremy Feldman <jeremykf@amazon.com>
2025-09-12 15:57:05 -0400

    additional license comments

--- lma-ai-stack/source/appsync/endVirtualParticipant.js
+++ lma-ai-stack/source/appsync/endVirtualParticipant.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Amazon.com
+ * This file is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
+ */
 /**
  * AppSync JavaScript Resolver for endVirtualParticipant
  * Ends a Virtual Participant and triggers Lambda for enhanced processing
--- lma-ai-stack/source/lambda_functions/virtual_participant_manager/error_analyzer.py
+++ lma-ai-stack/source/lambda_functions/virtual_participant_manager/error_analyzer.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Amazon.com
+# This file is licensed under the MIT License.
+# See the LICENSE file in the project root for full license information.
+
 """
 Virtual Participant Error Analyzer
 Provides intelligent error categorization and troubleshooting recommendations
