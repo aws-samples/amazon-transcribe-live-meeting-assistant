6327f4dba9438ff63bbd0d872ab2bbd3794aac18
Jeremy Feldman <jeremykf@amazon.com>
2026-03-18 16:39:50 +0000

    remove character

--- lma-nova-sonic-config-stack/source/lambda_functions/nova_sonic_config_upload.py
+++ lma-nova-sonic-config-stack/source/lambda_functions/nova_sonic_config_upload.py
@@ -1,4 +1,4 @@
-v# Copyright (c) 2025 Amazon.com
+# Copyright (c) 2025 Amazon.com
 # This file is licensed under the MIT License.
 # See the LICENSE file in the project root for full license information.
 
